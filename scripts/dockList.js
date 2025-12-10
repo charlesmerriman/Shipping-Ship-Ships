fa6b077c87866e5731f580fecd904a442af31ff1
charlesmerriman <zacmerr@gmail.com>
2025-12-10 11:26:07 -0600

    Test change

--- scripts/dockList.js
+++ scripts/dockList.js
@@ -1,27 +1,27 @@
-import { getDocks, getHaulingShips } from "./database.js";
+import { getDocks, getHaulingShips } from "./database.js"
 
-const docks = getDocks();
-const haulingShips = getHaulingShips();
+const docks = getDocks()
+const haulingShips = getHaulingShips()
 
 document.addEventListener("click", (clickEvent) => {
-  const itemClicked = clickEvent.target;
-  if (itemClicked.dataset.type === "dock") {
-    const dockId = itemClicked.dataset.id;
-    let ships = [];
-    for (const haulingShip of haulingShips) {
-      if (haulingShip.dockId === parseInt(dockId)) {
-        ships.push(haulingShip.name);
-      }
-    }
-    if (ships.length === 0) {
-      window.alert(`${itemClicked.innerHTML} is currently unloading nothing`);
-    } else {
-      window.alert(
-        `${itemClicked.innerHTML} is currently unloading ${ships.join(", ")}`
-      );
-    }
-  }
-});
+	const itemClicked = clickEvent.target
+	if (itemClicked.dataset.type === "dock") {
+		const dockId = itemClicked.dataset.id
+		let ships = []
+		for (const haulingShip of haulingShips) {
+			if (haulingShip.dockId === parseInt(dockId)) {
+				ships.push(haulingShip.name)
+			}
+		}
+		if (ships.length === 0) {
+			window.alert(`${itemClicked.innerHTML} is currently unloading nothing`)
+		} else {
+			window.alert(
+				`${itemClicked.innerHTML} is currently unloading ${ships.join(", ")}`
+			)
+		}
+	}
+})
 
 // When dock is clicked, present a message to the user that follows:
 //  The Shanghai, China dock is currently unloading Boaty McBoatface
@@ -33,14 +33,16 @@ document.addEventListener("click", (clickEvent) => {
 // The Shanghai, China dock is currently unloading Boaty McBoatface, Seawise Giant
 
 export const docksList = () => {
-  let docksHTML = "<ul>";
+	let docksHTML = "<ul>"
 
-  for (const dock of docks) {
-    // Convert each dock object to an <li> and append to the docksHTML string
-    docksHTML += `<li data-id="${dock.id}" data-dockVolume="${dock.volume}" data-type="dock">${dock.location}</li>`;
-  }
+	for (const dock of docks) {
+		// Convert each dock object to an <li> and append to the docksHTML string
+		docksHTML += `<li data-id="${dock.id}" data-dockVolume="${dock.volume}" data-type="dock">${dock.location}</li>`
+	}
 
-  docksHTML += "</ul>";
+	docksHTML += "</ul>"
 
-  return docksHTML;
-};
+	return docksHTML
+}
+
+// test change
